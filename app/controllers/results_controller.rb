32e541c0a4cf3c10949bbed6036dbcb5415a69fd
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-04-18 14:09:58 +0900

    コントローラーの編集

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,6 +9,19 @@ class ResultsController < ApplicationController
     @result = Result.new
   end
 
+  def create
+    @result = Result.new(result_params)
+    @result.user_id = current_user.id
+    @result.save
+    redirect_to result_path(@result)
+  end
+
   def edit
   end
+
+  private
+  def result_params
+    params.require(:result).permit(:title, :body, :image)
+  end
+
 end
