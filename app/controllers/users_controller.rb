79e2ec36de4fa475322ea2faee3fc6c116c0106b
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-04-18 12:25:59 +0900

    簡単なエラー直し（コントローラー）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user User.find(params[:id])
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
